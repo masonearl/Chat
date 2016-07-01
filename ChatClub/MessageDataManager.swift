e57aa8daa115fe2c4baf2eb12d487a5471da5b4b
buildmase <mason.earl@icloud.com>
2016-07-01 15:51:10 -0600

    working
    yupppp

---
+++ ChatClub/MessageDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  MessageDataManager.swift
+//  MorseChat
+//
+//  Created by Mason Earl on 7/1/16.
+//  Copyright © 2016 trianglez. All rights reserved.
+//
+
+import Foundation
---
+++ ChatClub/MessageObject.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  ChatClub
+//
+//  Created by Mason Earl on 6/30/16.
+//  Copyright © 2016 trianglez. All rights reserved.
+//
+
+import CloudKit
+
+class Message {
+    
+}
+
